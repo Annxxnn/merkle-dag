e14d450ef76a2bee4de68ca0c4dba5c15c3328e0
Annxxnn <132678447+Annxxnn@users.noreply.github.com>
2024-03-13 10:35:40 +0800

    modify dag.go

--- file.go
+++ file.go
@@ -1,27 +1,38 @@
 package merkledag
 
+// 定义了一个常量 FILE 和 DIR，分别表示文件和目录的类型
 const (
 	FILE = iota
 	DIR
 )
 
+// Node 接口，表示 MerkleDag 中的节点。它包含两个方法：
+// Size() 方法返回节点的大小（以字节为单位）。
+// Type() 方法返回节点的类型（FILE 或 DIR）。
 type Node interface {
 	Size() uint64
+	Name() string
 	Type() int
 }
 
+// File 接口，表示 MerkleDag 中的文件节点。它继承了 Node 接口，并添加了一个方法：
+// Bytes() 方法返回文件节点的内容（以字节数组的形式）。
 type File interface {
 	Node
-
 	Bytes() []byte
 }
 
+// Dir 接口，表示 MerkleDag 中的目录节点。它继承了 Node 接口，并添加了一个方法：
+// It() 方法返回一个 DirIterator 对象，用于遍历目录中的子节点。
 type Dir interface {
 	Node
 
 	It() DirIterator
 }
 
+// DirIterator 接口，表示目录迭代器，用于遍历目录中的子节点。它包含两个方法：
+// Next() 方法用于移动到目录中的下一个子节点，如果存在下一个子节点则返回 true，否则返回 false。
+// Node() 方法返回当前指向的子节点。
 type DirIterator interface {
 	Next() bool
 
--- kvstore.go
+++ kvstore.go
@@ -1,5 +1,9 @@
 package merkledag
 
+// Has(key []byte) (bool, error)：检查存储中是否存在给定的键。如果存在，返回 true，否则返回 false。如果在检查过程中发生错误，返回错误信息。
+// Put(key, value []byte) error：将键值对存储到存储系统中。使用给定的键和值进行存储。如果存储过程中发生错误，返回错误信息。
+// Get(key []byte) ([]byte, error)：根据给定的键从存储中检索对应的值。如果找到对应的值，则返回该值的字节数组。如果在检索过程中发生错误或找不到对应的值，返回错误信息。
+// Delete(key []byte) error：根据给定的键从存储中删除对应的键值对。如果删除成功，返回 nil。如果在删除过程中发生错误，返回错误信息。
 type KVStore interface {
 	Has(key []byte) (bool, error)
 	Put(key, value []byte) error
